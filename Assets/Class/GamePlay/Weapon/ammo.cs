47fa7cd3016121e3b56aaf64f98ea9ccc80516f0
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-12-12 23:23:08 +0700

    add weapon line predited

--- Assets/Class/GamePlay/Weapon/ammo.cs
+++ Assets/Class/GamePlay/Weapon/ammo.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class ammo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject shootingLine;
+    private GameObject[] tempLine = new GameObject[1000];
+    private int count = 0;
+    
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "wall"){
             Destroy(gameObject);
