87971b54bc0cafb3f3a3cd6a5d1f18065f056afc
ToeiKanta <41673034+ToeiKanta@users.noreply.github.com>
2019-12-13 18:17:16 +0700

    add enemy control amd damage

---
+++ Assets/Class/GamePlay/Enemy/EnemyControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EnemyControl : MonoBehaviour
+{
+    [SerializeField]
+    private int health = 30;
+    [SerializeField]
+    private int headDamage = 10;
+    [SerializeField]
+    private int bodyDamage = 5;
+    [SerializeField]
+    private TextMeshProUGUI healthBar;
+    [SerializeField]
+    private TextMeshProUGUI healthDiffTxt;
+    private bool isDie = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateHealthBar();
+        healthDiffTxt.text = "";
+    }
+
+    public void takeDamage(string part){
+        int damageTaken = 0;
+        switch(part){
+            case "Head":
+                damageTaken = headDamage;break;ใช่
+            case "Body":
+                damageTaken = bodyDamage;break;
+        }
+        health -= damageTaken;
+        if(health <= 0){
+            health = 0;
+            Debug.Log("DEAD");
+            isDie = true;
+        }
+        StartCoroutine(showHealthDiff(-damageTaken));
+        updateHealthBar();
+    }
+
+    IEnumerator showHealthDiff(int healthDiff){
+        healthDiffTxt.text = healthDiff.ToString();
+        yield return new WaitForSeconds(0.7f);
+        healthDiffTxt.text = "";
+    }
+    void updateHealthBar(){
+        healthBar.text = "HP : " + health.ToString();
+    }
+
+}
--- Assets/Class/GamePlay/Weapon/ammo.cs
+++ Assets/Class/GamePlay/Weapon/ammo.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class ammo : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private bool isColission = false;
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        transform.up = (GetComponent<Rigidbody2D>().velocity);
-    }
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.tag == "wall"){
-            Destroy(gameObject);
+        if(!isColission){
+            transform.up = rb.velocity;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("wall")){
+            Destroy(gameObject);
+        }else if(other.CompareTag("Enemy")){
+            // Debug.Log("Hit");
+            isColission = true;
+            stick();
+            takeDamage(other);
+        }
+    }
+    private void takeDamage(Collider2D other){
+        var enemy = other.gameObject.GetComponentInParent<EnemyControl>();
+        if (other.gameObject.name == "Head"){
+            enemy.takeDamage("Head");
+        }
+        if (other.gameObject.name == "Body"){
+            enemy.takeDamage("Body");
+        }
+    }
+    private void stick(){
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
     }
 }
